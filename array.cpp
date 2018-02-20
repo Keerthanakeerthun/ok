bf73cafca284aa60b932b01de123a0e255cad7e2
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-02-20 08:01:11 -0800

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,k,i,sum=0,a[200];
+	cout<<"Enter the two numbers"<<endl;
+	cin>>n>>k;
+	for(i=1;i<=n;i++)
+	{
+		cout<<i<<endl;
+	}
+	i=a[200];
+	for(i=1;i<=k;i++)
+	{
+		sum=sum+i;
+	}
+	cout<<"the sum is "<<sum<<endl;
+	// your code goes here
+	return 0;
+}
