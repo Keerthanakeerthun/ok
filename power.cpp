caa985b72e379cd7a86f2f989f11e1536fa73875
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-02-21 06:43:03 -0800

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+
+int main() 
+{
+    float base, exponent, result;
+
+    cout << "Enter base and exponent values  ";
+    cin >> base >> exponent;
+
+    result = pow(base, exponent);
+
+    cout << base << "^" << exponent << " = " << result;
+    
+    return 0;
+}
