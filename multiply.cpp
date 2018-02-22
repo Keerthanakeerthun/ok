a7dbf561e7c4632a147bc6cdc4c3ed272b5f56e2
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-02-22 07:18:20 -0800

    Create multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int num,i;
+  cout<<"Enter a  positive number"<<endl;
+  cin>>num;
+ 
+  for(i=1;i<=5;i++)
+  {
+
+  
+  cout<<num<<" * "<<i<<" = "<<num*i<<endl;
+  
+  }
+ 
+  return 0;
+}
+  
