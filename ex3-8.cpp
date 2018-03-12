942d078c910d1f20436bb81b7d30b74b61afa879
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-12 08:40:56 -0700

    Create ex3-8.cpp

---
+++ ex3-8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2,temp;
+cout<<"Enter the two numbers"<<endl;
+cin>>num1>>num2;
+    cout << "Numbers before swapping" << endl;
+    cout << "number 1 = " << num1 << ", number 2 = " << num2 << endl;
+
+    num1 = num1 + num2;  
+  num2= num1 - num2;  
+  num1= num1 - num2;  //using arithmetic operators
+
+
+    cout << " Numbers after swapping" << endl;
+    cout << "number 1 = " << num1 << ", number 2 = " << num2 << endl;
+
+    return 0;
+}
