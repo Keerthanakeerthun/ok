3289979cc64ecab06abb5f7445ec82fc35bf69ba
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-12 08:44:17 -0700

    Update ex3-8.cpp

--- ex3-8.cpp
+++ ex3-8.cpp
@@ -9,10 +9,9 @@ cin>>num1>>num2;
     cout << "Numbers before swapping" << endl;
     cout << "number 1 = " << num1 << ", number 2 = " << num2 << endl;
 
-    num1 = num1 + num2;  
-  num2= num1 - num2;  
-  num1= num1 - num2;  //using arithmetic operators
-
+   num1 = num1 ^ num2;  
+  num2 = num1 ^ num2;  
+  num1 = num1 ^ num2; 
 
     cout << " Numbers after swapping" << endl;
     cout << "number 1 = " << num1 << ", number 2 = " << num2 << endl;
