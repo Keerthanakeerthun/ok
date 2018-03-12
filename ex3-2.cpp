74bef508ed3579f83105eff26bce6efb6bfe69d0
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-12 07:25:45 -0700

    Create ex3-2.cpp

---
+++ ex3-2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdio.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    char s[1000];
+    int i,count=1;
+    
+    cout<<"Enter a string:";
+    gets(s);
+    
+    for(i=0;a[i]!='\0';++i)
+    {
+        if(a[i]==' ')
+            count++;
+    }
+    
+    cout<<"The totalnumber of words in the given string is "<<count<<endl;
+ 
+    return 0;
+}
