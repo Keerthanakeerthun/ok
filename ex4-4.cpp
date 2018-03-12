854e26e644996d966d7322fe2eb57c725cae268b
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-12 10:06:38 -0700

    Create ex4-4.cpp

---
+++ ex4-4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cout<<"Enter the number "<<endl;
+	cin>>n;
+
+	
+		if(n>=1&&n<=10)
+
+
+	{
+		cout<<"The number "<<n<<" lies between 1 and 10"<<endl;
+	}
+	
+	else
+	{
+		cout<<"Enter valid input"<<endl;
+	}
+	return 0;
+}
