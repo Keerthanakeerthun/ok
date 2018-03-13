998c522c333f503f189d289aed40d1ff55fd7599
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-12 19:11:48 -0700

    Create ex6-8.cpp

---
+++ ex6-8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,res;
+cout<<"Enter a number"<<endl;
+cin>>num1;
+res=num%7;
+if(res==0)
+{
+cout<<num<<" is multiple of 7"<<endl;
+}
+return 0;
+}
