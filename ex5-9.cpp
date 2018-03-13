570d9f65ee9754a3822527e6c6973b0f2663df79
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-12 18:47:24 -0700

    Update ex5-9.cpp

--- ex5-9.cpp
+++ ex5-9.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main()
 {
-    int i,n,a[i],max;
+    int i,num,a[i],large;
     cout<<"Enter the total elements  of the array"<<endl;
-    cin>>n;
+    cin>>num;
     cout<<"Enter the elements in  array"<<endl;
-    for(i=0;i<n;i++){
+    for(i=0;i<num;i++){
         cin>>a[i];
     }
-    max = a[0];
-    for(i=1;i<n;i++){
-        if(max<a[i]){
-            max = a[i];
+    large = a[0];
+    for(i=1;i<num;i++){
+        if(large<a[i]){
+            large = a[i];
         }
     }
-    cout<<"Largest number in the array  is:"<<max;
+    cout<<"Largest number in the array  is:"<<large;
 
     return 0;
 }
