d2d5521dc5562493fe89e6c54b440b0ddb2f8ede
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-12 18:30:48 -0700

    Create ex5-5.cpp

---
+++ ex5-5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int num1,num2,pro;
+ 
+    cout << "Enter two numbers : "<<endl;
+    cin >> num1>>num2;
+    pro=num1*num2;
+   
+ if(pro>0&&pro%2==0)
+    {
+        cout<<even;
+    }
+    else
+   
+    cout << "odd"<<endl;
+         return 0;
+}
