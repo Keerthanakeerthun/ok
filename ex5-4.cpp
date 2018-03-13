8efe191b53b5179758f924f259c62baad087c5ae
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-12 18:26:46 -0700

    Create ex5-4.cpp

---
+++ ex5-4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int num,res;
+ 
+    cout << "Enter the number : "<<endl;
+    cin >> num;
+   
+ if(num>0&&num%2==0)
+    {
+        cout<<num;
+    }
+    else
+    res=num-1;
+    cout << ""The nearest even number is "<<res<<endl;
+         return 0;
+}
+© 2018 GitHub, Inc.
