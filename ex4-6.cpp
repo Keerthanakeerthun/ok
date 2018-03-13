2b9cf52708b6862b55ae00d3901baaa23dbfd9ee
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-12 17:45:02 -0700

    Create ex4-6.cpp

---
+++ ex4-6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num,next;
+cout<<"Enter a number"<<endl;
+cin>>num;
+if(num>0&&num<=100000000000)
+{
+next=num+1;
+}
+cout<<"the next number is "<<next<<endl;
+return 0;
+}
