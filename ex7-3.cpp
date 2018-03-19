e8b7f120e745daeee012747863a35dea9157395c
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-19 14:39:50 +0530

    Update ex7-3.cpp

--- ex7-3.cpp
+++ ex7-3.cpp
@@ -1,1 +1,29 @@
-largest between the range
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+{   
+    int l,r,a[100];
+    int n,i,count=0;
+  cout<<"Enter the number to find"<<endl;
+  cin>>n;
+    cout<<"Enter the numbers"<<endl;
+    cin>>l>>r;
+
+    for(i=l;i<r;++i)
+    {
+        if(i=n)
+        {
+            count+=1;
+            cout<<"yes"<<endl;
+        }
+   
+    else
+       {
+    	cout<<"No"<<endl;
+       }
+    
+    }
+	return 0;
+}
