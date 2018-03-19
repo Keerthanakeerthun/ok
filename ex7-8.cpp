4a7621af440400b6b6a2d0e5a6328359cc7bd577
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-19 14:41:58 +0530

    Update ex7-8.cpp

--- ex7-8.cpp
+++ ex7-8.cpp
@@ -1,1 +1,25 @@
-8 in cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+{   
+    int n,res
+   
+  cout<<"Enter the number"<<endl;
+  cin>>n;
+    
+        if(n%13==0)
+        {
+            
+            cout<<"yes"<<endl;
+        }
+   
+    else
+       {
+    	cout<<"No"<<endl;
+       }
+    
+    
+	return 0;
+}
