01b4e64010623073734deacecb7051d6ee444e01
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-19 14:30:07 +0530

    Update ex7-2.cpp

--- ex7-2.cpp
+++ ex7-2.cpp
@@ -1,1 +1,27 @@
-vowel or not
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+{   
+    char s[100];
+    int n,i,count=0;
+    cout<<"Enter any string"<<endl;
+    cin>>s;
+    n=strlen(s);
+    for(i=0;i<n;++i)
+    {
+        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+        {
+            count+=1;
+            cout<<"yes"<<endl;
+       }
+   
+    else
+    {
+    	cout<<"No"<<endl;
+    }
+    
+}
+	return 0;
+}
