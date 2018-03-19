6ee245f275d1edab22d87453b256980146f80aca
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-19 15:44:18 +0530

    Update ex8-4.cpp

--- ex8-4.cpp
+++ ex8-4.cpp
@@ -1,1 +1,11 @@
-4 in 8
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ char ch;
+ cout << "Enter a character: "<<endl;
+ cin >> ch;
+ cout << "ASCII Value  is " << int(ch);
+ return 0;
+}
