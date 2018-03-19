231cfe2e949533c93a83ae57a385699aa417205b
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-19 14:55:01 +0530

    Update ex7-9.cpp

--- ex7-9.cpp
+++ ex7-9.cpp
@@ -1,1 +1,50 @@
-9 in c
+ #include <iostream>
+using namespace std;
+
+int main() {
+	
+    int num1,num2,res;
+  
+  cout<<"Enter the numbers"<<endl;
+  cin>>num1>>num2;
+   res=num1*num2;
+  if(num1==num2)
+  {
+    cout<<res<<" is a perfect square";
+  }
+  
+    
+        
+    else
+       {
+    	cout<<"No"<<endl;
+       }
+    
+    
+	return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    int num1,num2,res;
+  
+  cout<<"Enter the numbers"<<endl;
+  cin>>num1>>num2;
+   res=num1*num2;
+  if(num1==num2)
+  {
+    cout<<res<<" is a perfect square";
+  }
+  
+    
+        
+    else
+       {
+    	cout<<"No"<<endl;
+       }
+    
+    
+	return 0;
+}
