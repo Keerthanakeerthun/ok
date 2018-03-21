152376f0e8f6f6a2092d775be46b41599ede9031
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-21 11:42:29 +0530

    Create 92.cpp

---
+++ 92.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float C,K;
+    cout<<"Celcius : ";
+    cin>>C;
+    K=C+273.15;
+    cout<<"Kelvin : "<<K<<endl;
+    
+           
+}
