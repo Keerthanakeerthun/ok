df54a77bf20dc93adeb6ca4aa1bc287fcd45f5ea
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-21 11:28:28 +0530

    Create 91.cpp

---
+++ 91.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter The Numbers:";
+    cin>>a>>b>>c;
+cout<<a*b*c<<endl;
+    cout<<2*a*b+2*b*c+2*a*c;
+           
+}
