0e61e5c6fc41a0c0e8a5395976eff69322da50cc
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-03-26 14:19:16 +0530

    Create 95.cpp

---
+++ 95.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+float P,T,R,I;
+cout<<"Enter the P,T,R value for simple interest"<<endl;
+cin>>P>>T>>R;
+I=P*T*R/100;
+cout<<I<<endl;
+    return 0;
+}
