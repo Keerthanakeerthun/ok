53a70f1fc8d0cee6e6471baa58f38875fe9388ec
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-04-02 16:24:13 -0700

    Create 31.cpp

---
+++ 31.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long d,n,a,A;
+    float res;
+    cout<<"Enter n ";
+    cin>>n;
+    cout<<"Enter a ";
+    cin>>a1;
+    cout<<"Enter d ";
+    cin>>d;
+    A=a1+(n-1)*d;
+    res=n*(a1+an)/2.0;
+    cout<<endl<<sum<<endl;
+    return 0;
+}
