b9a0da0aae2a9f7c2fcc16f96de6721fb060d2d0
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-04-02 17:07:06 -0700

    Update 26.cpp

--- 26.cpp
+++ 26.cpp
@@ -7,37 +7,37 @@ int partition(int[],int,int);
  
 int main()
 {
-    int a[50],n,i;
-    cout<<"HOW MANY ELEMENTS?";
+    int arr[50],n,i;
+    cout<<"enter the number of elements";
     cin>>n;
     
     for(i=0;i<n;i++)
-        cin>>a[i];
+        cin>>arr[i];
         
-    quick_sort(a,0,n-1);
-    cout<<"\nArray after sorting:";
+    quick_sort(arr,0,n-1);
+    cout<<"Array after sorting:";
     
     for(i=0;i<n;i++)
-        cout<<a[i]<<" ";
+        cout<<arr[i]<<" ";
     
     return 0;        
 }
  
-void quick_sort(int a[],int l,int u)
+void quick_sort(int arr[],int l,int u)
 {
     int j;
     if(l<u)
     {
-        j=partition(a,l,u);
-        quick_sort(a,l,j-1);
-        quick_sort(a,j+1,u);
+        j=partition(arr,l,u);
+        quick_sort(arr,l,j-1);
+        quick_sort(arr,j+1,u);
     }
 }
  
-int partition(int a[],int l,int u)
+int partition(int arr[],int l,int u)
 {
     int v,i,j,temp;
-    v=a[l];
+    v=arr[l];
     i=l;
     j=u+1;
     
@@ -46,22 +46,22 @@ int partition(int a[],int l,int u)
         do
             i++;
             
-        while(a[i]<v&&i<=u);
+        while(arr[i]<v&&i<=u);
         
         do
             j--;
-        while(v<a[j]);
+        while(v<arr[j]);
         
         if(i<j)
         {
-            temp=a[i];
-            a[i]=a[j];
-            a[j]=temp;
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
         }
     }while(i<j);
     
-    a[l]=a[j];
-    a[j]=v;
+    arr[l]=arr[j];
+    arr[j]=v;
     
     return(j);
 }
