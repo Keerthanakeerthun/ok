ddbb013edbbc3d25e744a1756797ffa6ce4b473c
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-04-09 08:27:40 -0700

    Create 61.cpp

---
+++ 61.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    int i,k;
+    cout<<"Enter The String:";
+    cin>>s;
+    cout<<"Enter the number of characters"<<endl;
+    cin>>k;
+
+     for(i=0;i<k;i++)
+  {
+    cout<<s[i];
+  }
+    
+    return 0;
+ }
