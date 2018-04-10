7b4061bb0fab47bc41e622b3b5275455b52d3359
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-04-10 08:57:36 -0700

    Create 70.cpp

---
+++ 70.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int num,res;
+    cout<<"Enter the number : ";
+    cin>>num;
+    for(int i = 1; i < num; i++){
+        res = pow(2,i);
+        if(num < res)
+        {
+            cout<<res;
+            break;
+                    }
+    }
+    return 0;
+}
