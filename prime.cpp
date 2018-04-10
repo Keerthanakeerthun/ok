3f2c1b2e9276b39aff6bb1146948702a2db919f9
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-04-10 09:28:06 -0700

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -1,4 +1,8 @@
-int num,i,count=0;
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,i,count=0;
 	cout<<"Enter a number:";
 	cin>>num;
 	for(i=2;i<num;i++)
@@ -17,3 +21,5 @@ int num,i,count=0;
 	{
 		cout<<"This is not a prime number";
 	}
+return 0;
+}
