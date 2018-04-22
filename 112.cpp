dd4b9a4fabae5b03a5d633c24bf55f253abbae11
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-04-22 09:55:59 -0700

    Create 112.cpp

---
+++ 112.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,a[100],start,i,c;
+    cout<<"Enter the number of elements in the array : ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[100];
+    }
+    start = a[0];
+    for(i=0;i<n;i++)
+    {
+        if(start=a[100])
+        {
+            c=1;
+        }
+        else
+        c=0;
+    }
+    if(c=1)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else if(c=0)
+    {
+        cout<<"No"<<endl;
+    }
+    else
+    cout<<"Enter valid input"<<endl;
+    return 0;
+}
