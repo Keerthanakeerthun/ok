7e2106a10d691aa031566c57f1aece742e3dedff
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-04-22 09:14:38 -0700

    Create 110.cpp

---
+++ 110.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+   int K,sum=0;
+   cout<<"Enter K value within 100000 :";
+	cin>>N;
+ for(int i=1;i<=K;i++)
+ 
+  {
+	sum=sum+i;
+  }
+  cout<<sum;
+  
+  return 0;
+}
