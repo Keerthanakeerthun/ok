0c3e2998dfcd8f2dfcd6376eb6ee857106320e57
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-05-23 12:12:34 +0530

    Update and rename p8.cpp to p7.cpp

---
+++ p7.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str[20],tmp;
+     int i;
+     cin>>str;
+     for(i=0;i<strlen(str);i=i+2)
+     {
+     tmp = str[i];
+     str[i] = str[i+1];
+     str[i+1] = tmp;
+     }
+     cout<<str;
+return 0;
+}
